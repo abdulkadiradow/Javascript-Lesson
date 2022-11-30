11cd9df377a3b27e5267434853e1e1a6fdf35d91
Abdulkadir <abdul.adow1@gmail.com>
2022-11-30 20:50:58 +0000

    "naming variables"

---
+++ namingVariables.js
@@ -0,0 +1,32 @@
+
+// Naming Variables
+/*
+Variable names
+
+-cannot contain spaces.
+-must begin with a letter,
+   an underscore (_) or a dollar sign ($).
+-names can only contain letters,
+   nembers, underscore, or dollar signs.
+-names are case-sensitive.
+-cannot use reserved keywords.
+*/    
+
+
+var firstName = "Jamila";
+var age = 21;
+var isFemale = true;
+var balance = 100.32;
+var dob = new Date (2000, 0, 28);
+var person = {};
+var empty = "undifined";
+
+
+console.log (typeof firstName);
+console.log (typeof age);
+console.log (typeof isFemale);
+console.log (typeof balance);
+console.log (typeof dob);
+console.log (typeof person);
+console.log (typeof empty);
+
