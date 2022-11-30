a97970fd44d876efd4045e54c7d41a47ec17fd81
Abdulkadir <abdul.adow1@gmail.com>
2022-11-30 21:03:19 +0000

    "done"

--- namingVariables.js
+++ namingVariables.js
@@ -12,6 +12,25 @@ Variable names
 -cannot use reserved keywords.
 */    
 
+/* all reserved words
+abstract	arguments	await*	boolean
+break	byte	case	catch
+char	class*	const	continue
+debugger	default	delete	do
+double	else	enum*	eval
+export*	extends*	false	final
+finally	float	for	function
+goto	if	implements	import*
+in	instanceof	int	interface
+let*	long	native	new
+null	package	private	protected
+public	return	short	static
+super*	switch	synchronized	this
+throw	throws	transient	true
+try	typeof	var	void
+volatile	while	with	yield
+
+*/
 
 var firstName = "Jamila";
 var age = 21;
@@ -21,6 +40,21 @@ var dob = new Date (2000, 0, 28);
 var person = {};
 var empty = "undifined";
 
+var firstName = "Jamila";
+var age = 21;
+var isFemale = true;
+var balance = 100.32;
+var dob = new Date (2000, 0, 28);
+var person = {};
+var empty = "undifined";
+
+console.log (firstName);
+console.log (age);
+console.log (isFemale);
+console.log (balance);
+console.log (dob);
+console.log (person);
+console.log (empty);
 
 console.log (typeof firstName);
 console.log (typeof age);
