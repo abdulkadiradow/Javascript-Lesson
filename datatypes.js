70b07584e1d516aaca78f19955bf8f402a85f733
Abdulkadir <abdul.adow1@gmail.com>
2022-11-30 20:44:26 +0000

    "commit"

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+var firstName = "Jamila";
+var age = 21;
+var isFemale = true;
+var balance = 100.32;
+var dob = new Date (2000, 0, 28);
+var person = {};
+var empty = "undifined";
+
+
+console.log (typeof firstName);
+console.log (typeof age);
+console.log (typeof isFemale);
+console.log (typeof balance);
+console.log (typeof dob);
+console.log (typeof person);
+console.log (typeof empty);
+
