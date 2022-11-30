10a1c7fac77a4ecd337141433aa2dd3f2e2e772f
Abdulkadir <abdul.adow1@gmail.com>
2022-11-30 21:18:57 +0000

    "Strings"

---
+++ strings.js
@@ -0,0 +1,18 @@
+// Strings
+// string is a datatype that allows you to store a sequence of characters.
+
+var brand = "Amigoscode";
+console.log (typeof brand);
+console.log (brand.length);
+console.log (brand.toLocaleUpperCase())
+console.log (brand.substring(0, 6))
+
+var a = "Amigos";
+var b = "code";
+console.log (a + b);
+console.log (a + " " + b);
+console.log (`${a}${b}`);
+console.log (`${a} ${b}`);
+console.log (`${a}${b.toUpperCase()}`);
+console.log (`${a.toUpperCase()}${b}`);
+console.log (`${a.toUpperCase()}${b.toUpperCase()}`);
