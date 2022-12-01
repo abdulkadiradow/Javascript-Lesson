a2e6b71f5c903a057cc63022bf09d8c83af823db
Abdulkadir <abdul.adow1@gmail.com>
2022-12-01 00:32:20 +0000

    "ocjects"

---
+++ objects.js
@@ -0,0 +1,23 @@
+// Object is a collection of properties. each property have a value.
+
+
+var person = {
+    firstName: "Jamila",
+    age: 21,
+    isFemale: true,
+    balance: 100.32,
+    dob: new Date(2000, 0, 28).toJSON,
+
+    address: {
+
+      city: "London",
+      postCode: "SW9",
+    }
+};
+ console.log (person.firstName);
+ console.log (person.age);
+ console.log (person.balance);
+ console.log (person.address.city);
+ console.log (Object.values(person));
+ console.log (Object.keys(person));
+ console.log (JSON.stringify(person));
