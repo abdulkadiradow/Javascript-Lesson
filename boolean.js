66dc7a18d0e74910b3a59e63d8cd576b222c5101
Abdulkadir <abdul.adow1@gmail.com>
2022-12-06 01:24:01 +0000

    "booleons"

---
+++ boolean.js
@@ -0,0 +1,18 @@
+
+
+
+
+var isAdult = false;
+console.log (isAdult);
+
+if (isAdult) {
+  console.log("is adult")
+
+} else {
+  console.log ("is not an adult")
+}
+
+// You can use excalation mark to flip true and false
+
+console.log (!true);
+console.log (!false);
