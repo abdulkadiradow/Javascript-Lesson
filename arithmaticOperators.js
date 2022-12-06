46116bd93d6fb1390b42e4692f16ae744ca95094
Abdulkadir <abdul.adow1@gmail.com>
2022-12-06 02:31:02 +0000

    "done"

--- arithmaticOperators.js
+++ arithmaticOperators.js
@@ -1,2 +1,13 @@
 
 //
+var addition = 2 + 2;
+var subtraction = 2 - 2;
+var division = 10 / 2;
+var mutiplication = 10 * 2;
+var reminder = 10 % 4;
+
+console.log (addition);
+console.log (subtraction);
+console.log (division);
+console.log (mutiplication);
+console.log (reminder);
