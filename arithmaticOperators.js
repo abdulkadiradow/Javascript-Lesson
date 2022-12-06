ee1b03f18660c9ef09bd6ab0a06db72a481d3603
Abdulkadir <abdul.adow1@gmail.com>
2022-12-06 02:32:52 +0000

    "thanks"

--- arithmaticOperators.js
+++ arithmaticOperators.js
@@ -5,9 +5,11 @@ var subtraction = 2 - 2;
 var division = 10 / 2;
 var mutiplication = 10 * 2;
 var reminder = 10 % 4;
+var exponention = 3 ** 3; // means 3 times 3 times three times three 81
 
 console.log (addition);
 console.log (subtraction);
 console.log (division);
 console.log (mutiplication);
 console.log (reminder);
+console.log (exponention);
