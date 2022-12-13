3399c416b351e8b8ff491c4e8f0e069cd72ef91b
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-13 22:25:12 +0000

    "just check"

--- arithmaticOperators.js
+++ arithmaticOperators.js
@@ -1,11 +1,12 @@
+// Arithmatic Operators
 
-//
+
 var addition = 2 + 2;
 var subtraction = 2 - 2;
 var division = 10 / 2;
 var mutiplication = 10 * 2;
 var reminder = 10 % 4;
-var exponention = 3 ** 3; // means 3 times 3 times three times three 81
+var exponention = 3 ** 3; // means 3 times 3 times three times three 27
 
 console.log (addition);
 console.log (subtraction);
