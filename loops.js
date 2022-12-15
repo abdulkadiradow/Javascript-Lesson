0722343a74790a474c2352827c527d09911c1203
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-15 00:09:50 +0000

    "loops"

--- loops.js
+++ loops.js
@@ -1,2 +1,18 @@
 
- 
+ for (var i = 0; i <= 6; i++) {
+  console.log(i);
+ }
+
+
+ var names = [
+  "Abdul",
+  "Ali",
+  "Yusuf",
+  "Mohamed",
+  "Amin"
+ ];
+
+
+ for (var i = 0; i <= names.length; i++) {
+  console.log(names[i]);
+ }
