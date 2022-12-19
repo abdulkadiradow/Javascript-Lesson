2ea10ffc0614950e4fc0a8e2d5e83ac502d61cc6
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-19 23:41:27 +0000

    "while-loop"

--- while-loops.js
+++ while-loops.js
@@ -7,11 +7,31 @@ var names = [
   "Yonis"
  ];
 
- 
- 
+
+
+
+while(false){
+  console.log("hello")
+ }
+
+ var number = 0;
+ while (number < 5) {
+  console.log(number);
+  number = number + 1;
+ }
+
+
 
  var condition = true;
  while (condition) {
   console.log("hello");
   condition = false;
  }
+
+
+
+ var index = 0;
+ while (index < names.length){
+ console.log(names[index])
+ index = index + 1;
+}
