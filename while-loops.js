5818145c305431580dad8a3acaf9b75a3dfa3092
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-19 21:40:09 +0000

    "while-loops"

---
+++ while-loops.js
@@ -0,0 +1,17 @@
+var names = [
+  "Abdul",
+  "Ali",
+  "Yusuf",
+  "Mohamed",
+  "Amin",
+  "Yonis"
+ ];
+
+ 
+ 
+
+ var condition = true;
+ while (condition) {
+  console.log("hello");
+  condition = false;
+ }
