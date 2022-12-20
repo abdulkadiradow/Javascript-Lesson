207c4f61e1c8fb8f0e3b4caf4b1e5d5e29dcbd9d
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-20 00:51:28 +0000

    "increments-decrements

---
+++ increment-decrement-operator.js
@@ -0,0 +1,20 @@
+
+//postfix
+let number = 0;
+console.log("postfix")
+
+console.log(number++);
+console.log(number);
+console.log(number--);
+console.log(number);
+//prefix
+console.log("prefix");
+let numberTwo = 0;
+console.log(++numberTwo);
+console.log(++numberTwo);
+console.log(--numberTwo);
+//for loop
+console.log("for loop")
+for (let i = 0; i <= 10; i ++ ) {
+  console.log(i);
+}
