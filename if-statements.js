518055ec13df119f3ce0cb3b28d16dc6a3728f0b
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-20 02:01:24 +0000

    "if-statements"

---
+++ if-statements.js
@@ -0,0 +1,36 @@
+
+
+
+if (true) {
+  console.log("runs...");
+} 
+  console.log("not run...");
+
+  
+  
+  if (true) {
+    console.log("runs...");
+
+  } else {
+    console.log("not run...")
+  }
+
+
+  let condition = 1 < 0;
+
+  if (condition) {
+    console.log("runs...")
+  } else {
+    console.log("not run...")
+  }
+
+let gender = "M";
+if (gender == "M") {
+  console.log("Male");
+} else if (gender == "F") {
+  console.log("Female")
+} else {
+  console.log("Unknown");
+}
+
+
