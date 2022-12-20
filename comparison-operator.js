dfe04b0b35611fdc100aec09e1cbf4c6cd8067b1
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-20 01:31:32 +0000

    "comparison operators"

---
+++ comparison-operator.js
@@ -0,0 +1,16 @@
+
+// <  <=  >  >=  ==  !=
+
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+
+console.log("a" == "b");
+console.log("a" != "b");
+
+
+
+
