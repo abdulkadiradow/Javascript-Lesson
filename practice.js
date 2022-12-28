ddd7ff07e595000f5236650ce4525624d7abed92
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-28 22:43:30 +0000

    "save it"

--- practice.js
+++
@@ -1,9 +0,0 @@
-let currencyOne = 100;
-let currencyTwo = 0;
-let exchangeRate = 1.2;
-
-function myCurrency(amount, rate) {
-  return amount * rate;
-}
-currencyTwo = myCurrency(currencyOne, exchangeRate);
-console.log(currencyTwo);
