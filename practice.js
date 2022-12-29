6cc937c61b236000c324021f46a371d57024a56b
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-29 00:10:22 +0000

    '"save it"

--- practice.js
+++ practice.js
@@ -6,3 +6,10 @@ var num2 = 3;
 
 var total = getTotal(num1, num2);
 console.log(total);
+function getDistance(mph, h) {
+  return mph * h;
+}
+var mph = 60;
+var h = 2;
+var distance = getDistance(mph, h);
+console.log(distance);
